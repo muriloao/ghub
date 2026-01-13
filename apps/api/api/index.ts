40535cf22335be16e578451715ee5901f0bccba0
Murilo Oliveira <murilo.deoliveira@vitta.me>
2026-01-13 16:11:44 -0300

    feat: corrigir importação do AppModule e adicionar tsconfig para a API

--- apps/api/api/index.ts
+++ apps/api/api/index.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { NestExpressApplication } from '@nestjs/platform-express';
-import { AppModule } from './app.module';
+import { AppModule } from '../src/app.module';
 import { join } from 'path';
 import { VercelRequest, VercelResponse } from '@vercel/node';
 
